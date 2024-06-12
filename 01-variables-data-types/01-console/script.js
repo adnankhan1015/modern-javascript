504e53d803dbb95437d671e8902f013e03e55923
adnankhan <adnankhannad@gmail.com>
2024-06-12 23:57:43 +0500

    Add console

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,27 @@
+// Now we can show the stuff on the console with the console object in JavaScript
+
+console.log(100);
+
+console.log("Hello World");
+
+console.log(20, "Hello", true);
+
+const x = 100;
+
+console.log(x);
+
+console.error("Alert");
+
+console.warn("warning");
+
+console.table({ name: "Adnan", email: "adnan@gmail.com" });
+
+console.group("Simple");
+console.log(x);
+console.error("Alert");
+console.warn("warning");
+console.groupEnd();
+
+const styles = "padding: 10px; background-color: white; color: green";
+
+console.log("%cHello World", styles);

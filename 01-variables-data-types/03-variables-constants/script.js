596fa7a2afe362e04a5f9491f12bb1a3eb826d3a
adnankhan <adnankhannad@gmail.com>
2024-06-14 00:34:09 +0500

    Add variable

---
+++ 01-variables-data-types/03-variables-constants/script.js
@@ -0,0 +1,28 @@
+/*
+ * ways to declare a variable
+ * var, let, const
+ */
+
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName, lastName);
+
+let age = 30;
+
+console.log(age);
+
+//  Naming Conventions
+
+// - Can not start with a number
+
+// Multi-Word Formatting
+// firstName = camelCase
+// first_name = underscore
+// FirstName = PascalCase
+// firstname = lowercase
+
+// Re-assiging variables
+
+age = 31;
+console.log(age);

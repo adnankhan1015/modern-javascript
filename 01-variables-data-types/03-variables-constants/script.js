521064a21891841ff0fa6b8bde567370618f940c
adnankhan <adnankhannad@gmail.com>
2024-07-01 00:12:29 +0500

    variables-constant

--- 01-variables-data-types/03-variables-constants/script.js
+++ 01-variables-data-types/03-variables-constants/script.js
@@ -26,3 +26,46 @@ console.log(age);
 
 age = 31;
 console.log(age);
+
+let score;
+
+score = 1;
+
+console.log(score);
+
+if (true) {
+  score = score + 1;
+}
+console.log(score);
+
+const x = 100;
+
+// x = 200;
+
+let sport;
+
+const arr = [1, 2, 3, 4];
+arr.push(5);
+console.log(arr);
+
+const person = {
+  name: "John",
+  age: 32,
+  address: "address",
+};
+
+person.name = "Marry";
+
+person.email = "marry@gmail.com";
+
+console.log(person);
+
+// Declare multiple values at once
+
+let a, b, c;
+const d = 10,
+  e = 20,
+  f = 30;
+
+console.log(d);
+console.log(a);

92655cb2c67e8acd83c931591b1be44e6b8c0a6b
adnankhan <adnankhannad@gmail.com>
2024-07-28 20:55:22 +0500

    Data Types, Primitive n Ref Types, Type Conversion, Operators, Type Coercion

---
+++ 01-variables-data-types/05-primitive-vs-reference-types/script.js
@@ -0,0 +1,19 @@
+let name = "Sumaira";
+const age = 30;
+
+let newName = name;
+console.log(newName);
+
+newName = "Khan";
+console.log(newName);
+
+const person = { name: "Maqsood", age: 25 };
+console.log(person);
+
+const newPerson = person;
+console.log(newPerson);
+
+person.name = "Momin";
+
+console.log(person);
+console.log(newPerson);
---
+++ 01-variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,22 @@
+let amount = "100";
+
+amount = parseInt(amount);
+amount = amount.toString();
+amount = Number(amount);
+amount = String(amount);
+
+console.log(amount);
+
+let amount2 = 5.5;
+amount2 = parseFloat(amount2);
+console.log(amount2);
+
+let x = true;
+x = Number(x);
+x = Boolean(x);
+x = String(x);
+console.log(x);
+
+let str = "Hello";
+let num = Number(str);
+console.log(num);
